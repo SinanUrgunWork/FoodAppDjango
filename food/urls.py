8fee1c9ea9edd10dcce82dda5e162d335be3d9e4
sinan urgun <77778835+SinanUrgunWork@users.noreply.github.com>
2023-02-19 19:15:21 +0300

    added froms like add , update and delete

--- food/urls.py
+++ food/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('items/', views.items, name='items'),
     path('<int:item_id>/', views.detail, name='detail'),
+    #add items
+    path('add',views.create_item,name='create_item'),
+    #edit
+    path('update/<int:id>',views.update_item,name='update_item'),
+    #Delete
+    path('delete/<int:id>',views.delete_item,name='delete_item'),
 ]
